51286e4df4e9030152741ac54243216bcb255d13
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-04-18 18:00:42 +0000

    Complete form validations with js

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ window.onload = function (event) {
   submitButton.addEventListener('click', function (event) {
     event.preventDefault()
 
-    //First name validations
+    //Form firstname validations
 
     if (firstNameInputElement.value === "") {
       firstNameInputElement.style.backgroundColor = 'pink'
@@ -27,9 +27,9 @@ window.onload = function (event) {
       firstNameInputElement.style.backgroundColor = 'white'
       errorInForm = false
     }
-
-
-    if (firstNameInputElement.value.length < 5) {
+    //Name must have between range of 3 and 25 characters in length
+    if (firstNameInputElement.value.length < 2 || firstNameInputElement.value.length > 25) {
+      console.log("length of firstname is", firstNameInputElement.value.length)
       errorInForm = true
       firstNameInputElement.style.backgroundColor = 'pink'
     } else {
@@ -37,52 +37,108 @@ window.onload = function (event) {
       errorInForm = false
     }
 
-
+    //Form last name validations
     if (lastNameInputElement.value === "") {
       lastNameInputElement.style.backgroundColor = 'pink'
       errorInForm = true
     } else { lastNameInputElement.style.backgroundColor = 'white' }
 
+    //Last name must have between range of X and Y characters in length
+    if (lastNameInputElement.value.length < 2 || firstNameInputElement.value.length > 40) {
+      errorInForm = true
+      lastNameInputElement.style.backgroundColor = 'pink'
+    } else {
+      lastNameInputElement.style.backgroundColor = 'white'
+      errorInForm = false
+    }
+
+    //Form city name validations
     if (cityInputElement.value === "") {
       cityInputElement.style.backgroundColor = 'pink'
       errorInForm = true
     } else { cityInputElement.style.backgroundColor = 'white' }
 
+    if (cityInputElement.value.length < 2 || cityInputElement.value.length > 80) {
+      cityInputElement.style.backgroundColor = 'pink'
+      errorInForm = true
+    } else {
+      cityInputElement.style.backgroundColor = 'white'
+    }
+
+    //Form card number validations
     if (cardNumberInputElement.value === "") {
       cardNumberInputElement.style.backgroundColor = 'pink'
       errorInForm = true
     } else { cardNumberInputElement.style.backgroundColor = 'white' }
 
+    if (cardNumberInputElement.value.length !== 16) {
+      cardNumberInputElement.style.backgroundColor = 'pink'
+      errorInForm = true
+    } else {
+      cardNumberInputElement.style.backgroundColor = 'white'
+    }
+
+    //Form state validations
     if (stateInputElement.value === "Pick a state") {
       stateInputElement.style.backgroundColor = 'pink'
       errorInForm = true
     } else { stateInputElement.style.backgroundColor = 'white' }
 
+
+    //Form postal code validations
     if (postalCodeElement.value === "") {
       postalCodeElement.style.backgroundColor = 'pink'
       errorInForm = true
     } else { postalCodeElement.style.backgroundColor = 'white' }
 
+    if (postalCodeElement.value.length !== 5) {
+      postalCodeElement.style.backgroundColor = 'pink'
+      errorInForm = true
+    } else {
+      postalCodeElement.style.backgroundColor = 'white'
+    }
+
+
+    //Form card cvc validations
     if (inputCvcElement.value === "") {
       inputCvcElement.style.backgroundColor = 'pink'
       errorInForm = true
     } else { inputCvcElement.style.backgroundColor = 'white' }
 
+    if (isNaN(parseInt(inputCvcElement.value))) {
+      console.log(parseInt(inputCvcElement.value))
+      inputCvcElement.style.backgroundColor = 'pink'
+      errorInForm = true
+    } else { inputCvcElement.style.backgroundColor = 'white' }
+
+
+    //Form amount validations
     if (inputAmountElement.value === "") {
       inputAmountElement.style.backgroundColor = 'pink'
       errorInForm = true
     } else { inputAmountElement.style.backgroundColor = 'white' }
 
+    if (inputAmountElement.value === 0 || inputAmountElement.value < 0) {
+      inputAmountElement.style.backgroundColor = 'pink'
+      errorInForm = true
+    } else { inputAmountElement.style.backgroundColor = 'white' }
+
+
+    //Form message validations
     if (inputMessageElement.value === "") {
       inputMessageElement.style.backgroundColor = 'pink'
       errorInForm = true
     } else { inputMessageElement.style.backgroundColor = 'white' }
 
-    //if(masterCardInputElement)
+    if (inputMessageElement.value.length > 1000) {
+      inputAmountElement.style.backgroundColor = 'pink'
+      errorInForm = true
+    } else { inputAmountElement.style.backgroundColor = 'white' }
 
 
 
 
+    //Error message will apear indicating form is not correctly filled
     errorInForm ? alertDivElemnt.className = "alert alert-danger fw-medium" : alertDivElemnt.className = "invisible"
 
   })
@@ -118,7 +174,6 @@ window.onload = function (event) {
 
   let alertDivElemnt = document.querySelector('#alertDiv')
 
-  let masterCardInputElement = document.querySelector('#mastercard')
 
 
 };
